e1bc0c4b54e86a2ab1ee87251a2f746c7d34c3d6
Muhammad Al Farrel Elmaretza <alfarrelelmaretza@gmail.com>
2023-12-05 02:06:29 +0700

    First Commit

---
+++ app/Views/landing_page.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Pempek Elly</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?= base_url('assets/img/logo/logo.png') ?>" rel="icon">
+  <link href="<?= base_url('assets/img/apple-touch-icon.png') ?>" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?= base_url('assets/vendor/aos/aos.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendor/bootstrap-icons/bootstrap-icons.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendor/boxicons/css/boxicons.min.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendor/glightbox/css/glightbox.min.css') ?>" rel="stylesheet">
+  <link href="<?= base_url('assets/vendor/swiper/swiper-bundle.min.css') ?>" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?= base_url("assets/css/style.css")?>" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Appland
+  * Updated: Sep 25 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top  header-transparent ">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <a href="index.html"><img src="<?= base_url('/assets/img/logo/logo.png') ?>" alt="logo" class="img-fluid"></a>
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Beranda</a></li>
+          <li><a class="nav-link scrollto" href="#features">Tentang Kami</a></li>
+          <li><a class="nav-link scrollto" href="#gallery">Produk</a></li>
+          
+          <li><a class="nav-link scrollto" href="#footer">Kontak Kami</a></li>
+          <li><a class="getstarted scrollto btn btn-success" href="https://wa.me/6281367240036">Pesan Disini</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
+          <div>
+            <h1>Pempek Elly</h1>
+            <h2>Aneka Olahan Ikan</h2>
+            <a href="#gallery" class="btn btn-warning m-3 p-4"><i class="bx bxl-play-store"></i> Lihat Menu</a>
+            <a href="https://wa.me/6281367240036" class="btn btn-success m-3 p-4"><i class="bx bxl-whatsapp"></i> Pesan Disini</a>
+          </div>
+        </div>
+        <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img" data-aos="fade-up">
+          <img src="<?= base_url('assets/img/logo/home.png') ?>" class="img-fluid" alt="bg">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= App Features Section ======= -->
+    <section id="features" class="features">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-up">
+          <h2>Tentang Pempek Elly</h2>
+          <div class="row justify-content-center">
+            <div class="col-6"><p class="text-center me-5 p-5">Pempek Elly merupakan sebuah home industri yang spesialis dalam memproduksi berbagai jenis olahan ikan, mencakup ragam pempek, tekwan, otak-otak bakar, 
+             lenggang bakar, bakso ikan, siomay, dan produk lainnya. Semua produk yang dihasilkan oleh Pempek Elly dibuat dari daging ikan pilihan serta 
+             bahan-bahan premium berkualitas tinggi yang diolah dengan standar kebersihan yang tinggi.</p></div>
+
+            <div class="col-6"><p class="text-center ms-5 p-5"> Keunggulan utama dari Pempek Elly tidak hanya terletak 
+             pada kelezatan produk-produknya, tetapi juga pada perhatian khusus yang diberikan terhadap pemilihan bahan baku dan proses produksi yang higienis.
+             Dengan komitmen untuk menyajikan produk berkualitas, Pempek Elly menjadi pilihan yang tepat bagi pecinta makanan olahan ikan yang mengutamakan 
+             cita rasa dan keamanan konsumsi.</p></div>          
+          </div>
+          
+        </div>
+
+        <div class="container mt-3 text-center" data-aos="fade-up">
+          <h3>Berikut Kami Sisipkan Dokumen Perizinan Usaha</h3>
+          <div id="carouselExampleCaptions" class="carousel slide mx-auto border border-2" data-bs-ride="carousel" style="max-width: 450px;">
+        <div class="carousel-indicators">
+             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img src="<?= base_url('/assets/img/surat/sertifikat halal.jpg') ?>" class="d-block w-100" alt="Image 1">
+                          <div class="carousel-caption d-none d-md-block">
+                              <h5>Sertifikat Halal</h5>
+                          </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="<?= base_url('/assets/img/surat/perizinan umkm.jpg') ?>" class="d-block w-100" alt="Image 2">
+                        <div class="carousel-caption d-none d-md-block">
+                              <h5>Sertifikat Perizinan UMKM</h5>
+                          </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="<?= base_url('/assets/img/surat/sertifikat merk.jpg') ?>" class="d-block w-100" alt="Image 3">
+                        <div class="carousel-caption d-none d-md-block">
+                              <h5>Sertifikat Perizinan Merek</h5>
+                          </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img src="<?= base_url('/assets/img/surat/perizinan.jpg') ?>" class="d-block w-100" alt="Image 4">
+                        <div class="carousel-caption d-none d-md-block">
+                              <h5>Sertifikat Perizinan Usaha</h5>
+                          </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                       <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                       <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                       <span class="visually-hidden">Next</span>
+                </button>
+          </div>
+  </div>
+</div>
+
+      </div>
+    </section><!-- End App Features Section -->
+
+
+    <!-- ======= Gallery Section ======= -->
+    <section id="gallery" class="gallery">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title" data-bs-slide="fade-up">
+          <h2>Daftar Menu Pempek Elly</h2>
+        </div>
+        <div class="container mt-3">
+  <div class="row">
+    <!-- Card 1 -->
+    <div class="col-md-4 mb-4">
+      <div class="card h-70 p-5">
+        <img src="<?= base_url('/assets/img/produk/pempek campur.jpg') ?>" class="card-img-top" alt="Image 1">
+        <div class="card-body">
+          <h5 class="card-title m-3">Pempek Campur</h5>
+          <p class="card-text m-1">10 Adaan, 6 Lenjer, 4 Telor.</p>
+          <p class="card-text m-0"><strong>Harga:</strong> Rp. 55.000.</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Card 2 -->
+    <div class="col-md-4 mb-4">
+      <div class="card h-70 p-5">
+        <img src="<?= base_url('/assets/img/produk/pempek adaan.jpg') ?>" class="card-img-top" alt="Image 2">
+        <div class="card-body ">
+          <h5 class="card-title m-3 ">Pempek Adaan</h5>
+          <p class="card-text m-1 ">20 Pempek Adaan.</p>
+          <p class="card-text m-0 "><strong>Harga:</strong> Rp. 55.000.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 mb-4">
+      <div class="card h-70 p-5">
+        <img src="<?= base_url('/assets/img/produk/pempek campur dan kulit.jpg') ?>" class="card-img-top" alt="Image 3">
+        <div class="card-body">
+          <h5 class="card-title m-3">Pempek Campur Kulit</h5>
+          <p class="card-text m-1">5 Adaan, 5 Kulit, 5 Lenjer, 5 Telor</p>
+          <p class="card-text m-0"><strong>Harga:</strong> Rp. 55.000.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 mb-4">
+      <div class="card h-70 p-5">
+        <img src="<?= base_url('/assets/img/produk/pempek kulit.jpg') ?>" class="card-img-top" alt="Image 4">
+        <div class="card-body">
+          <h5 class="card-title m-3">Pempek Kulit</h5>
+          <p class="card-text m-1">5 Pempek Kulit.</p>
+          <p class="card-text m-0"><strong>Harga:</strong> Rp. 14.000/Kelipatan 5.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 mb-4">
+      <div class="card h-70 p-5">
+        <img src="<?= base_url('/assets/img/produk/pempek lenjer.jpg') ?>" class="card-img-top" alt="Image 5">
+        <div class="card-body">
+          <h5 class="card-title m-3">Pempek Lenjer</h5>
+          <p class="card-text m-1">5 Pempek Lenjer.</p>
+          <p class="card-text m-0"><strong>Harga:</strong> Rp. 14.000/Kelipatan 5.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-4 mb-4">
+      <div class="card h-70 p-5">
+        <img src="<?= base_url('/assets/img/produk/pempek telor.jpg') ?>" class="card-img-top" alt="Image 6">
+        <div class="card-body">
+          <h5 class="card-title m-3">Pempek Telor</h5>
+          <p class="card-text m-1">5 Pempek Telor.</p>
+          <p class="card-text m-0"><strong>Harga:</strong> Rp. 14.000/Kelipatan 5.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+      </div>
+
+      
+
+      </div>
+    </section><!-- End Gallery Section -->
+
+    
+
+    
+ 
+  
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-5 col-md-6 footer-contact text-center ms-5 p-5">
+            <h3>Pempek Elly</h3>
+            <p>
+              Perumahan Taman Gunter II Blok A No.24<br>
+              Kemiling, Bandar Lampung<br>
+              Lampung, Indonesia<br><br>
+              <strong>Phone:</strong> 6281367240036<br>
+            </p>
+          </div>
+
+          
+
+        
+
+          <div class="col-lg-5 col-md-6 footer- text-center ms-5 p-5">
+            <h4>Mampir Ke Social Media Kita</h4>
+            <p>Selalu up-to-date dengan kelezatan terbaru dari Pempek Elly! 🌟 Mampir ke sosial media kami untuk melihat berbagai macam update, 
+              promo menarik, dan tentu saja, sajian istimewa kami yang selalu membuat lidah Anda bergoyang! 😋📲 #PempekElly #LezatBikinNgiler</p>
+            <div class="social-links mt-3">
+              <a href="#" class="tiktok"><i class="bx bxl-tiktok"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/ellysuarni27" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="https://wa.me/6281367240036" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Appland</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#navbar" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="<?= base_url("assets/vendor/aos/aos.js")?> "></script>
+  <script src="<?= base_url("assets/vendor/bootstrap/js/bootstrap.bundle.min.js")?> "></script>
+  <script src="<?= base_url("assets/vendor/glightbox/js/glightbox.min.js")?> "></script>
+  <script src="<?= base_url("assets/vendor/swiper/swiper-bundle.min.js")?> "></script>
+  <script src="<?= base_url("assets/vendor/php-email-form/validate.js") ?> "></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
